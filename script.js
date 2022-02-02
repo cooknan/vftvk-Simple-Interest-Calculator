bf301f5fe377bbce3ca21dacadf36fa56bf290b4
Nan Cook <cooknan1@gmail.com>
2022-02-02 16:41:41 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-var principal = document.getElementById("principal").value;
 
+//set up variables to get data entered by user
+
+var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
-
-var rate = document.getElementsById("rate").value;
-var years = document.getElementsById("years").value;
-
 var interest = principal * years * rate /100;
 var year = new Date().getFullYear()+parseInt(years);
 
 function updateRate()
 {
-    var rateeval = document.getElementById("rate").value; document.getElementById("rate_val").innerText=rateval;
+    var rateval = document.getElementById("rate").value; 
+	document.getElementById("rate_val").innerText=rateval;
 }
 
 function compute()
 {
     p = document.getElementById("principal").value;
-    
 }
         
