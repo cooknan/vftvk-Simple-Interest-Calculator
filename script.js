7da12ea0df1f432ce0890a54882247ed0b02c753
Nan Cook <cooknan1@gmail.com>
2022-02-02 12:13:43 -0500

    start
    start here

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * rate /100;
+var year = new Date().getFullYear()+parseInt(years);
+
+function updateRate()
+{
+    var rateeval = document.getElementById("rate").value; document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
     p = document.getElementById("principal").value;
