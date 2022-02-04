29f77d9aad28696357c412635ecc039109ffc8bd
Nan Cook <cooknan1@gmail.com>
2022-02-04 14:35:08 -0500

    2/4
    more changes probably worse

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 
 //set up variables to get data entered by user
 
-
 var principal = document.getElementById("principal").value;
 var rate = document.getElementById("rate").value;
 var years = document.getElementById("years").value;
@@ -14,29 +13,28 @@ function updateRate(){
 	document.getElementById("rate_val").innerText=rateval;
 }
 
-function checkdata()
-{
-
 //Check if principal field has a 0 or negative number 
-//First see what is in the field and stuff into var principal
-// if it looks good run function compute()
+//If so stop and refocus to principal field
 
-
-	if(principal <= 0 ){
-		alert("Please enter a positive number");
+function checkdata(){
+	if(principal <=0){
+		alert("Enter a positive number");
 		principal.focus();
 		return false;
-	}
-	if all is well return true and keep going
-	alert("Continue");
-	return true;
+		//else all is well returen true
+		return true:
+}
+}
 
 // now finally do the calc to show result to user
+
+//need to figure out how to trigger this one
+
 function compute()
 {
 	document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
-}
+
 
 
 
