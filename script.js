b8d09fa4447792df06f937d8a516f26d7cdd84e9
Nan Cook <cooknan1@gmail.com>
2022-02-05 16:54:10 -0500

    2/5 last update today
    Complete rework, still not working

--- script.js
+++ script.js
@@ -1,47 +1,29 @@
-
-//set up variables to get data entered by user
-
-var principal = document.getElementById("principal").value;
-var rate = document.getElementById("rate").value;
-var years = document.getElementById("years").value;
-var interest = principal * years * rate /100;
-var year = new Date().getFullYear()+parseInt(years);
-
-//give a value to right of slider as rate is updated
-function updateRate()
-{
- var rateval = document.getElementById("rate").value;
- document.getElementById("rate_val").innerText=rateval;
-}
-
-
-/*Check if principal field has a 0 or negative number 
-//If so stop and refocus to principal field
-/*
-function checkdata()
-{
-	if(principal <= 0){
-		alert("Enter a positive number");
-		principal.focus();
-		return false;
-	}
-	//all is well form validation is successful
-	alert("all set with principal")
-		return true;
-}
-		
-
-
- now finally do the calc to show result to user
-
-need to figure out how to trigger this one
-
 function compute()
 {
-ocument.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+var principal = document.getElementById("principal").value;
+	if(principal<1){
+		alert("Enter a positive number");
+		return false;
+		console.log(principal);
+	}
+	
+	var rate = document.getElementById("rate").value;
+	console.log(rate);
+	var years = document.getElementById("years").value;
+	console.log(years);
+	var interest = principal * years * rate /100;
+	console.log(interest);
+	var year = new Date().getFullYear()+parseInt(years);
+	console.log(year);
+
+
+var res = "If you deposit <mark>"+principal+",</mark>\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>";
+document.getElementById("result").innerHTML = res;
+console.log("inner html executed");
 }
 
-*/
-
-
-    
+function updateRate()
+{
+	var rateval = document.getElementById("rate").value;
+	document.getElementById("rate_val").innerText=rateval;
+}
